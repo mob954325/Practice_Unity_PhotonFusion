4def680e56fb669fccb0414dc169d27866f5abb0
mob954325 <lsh954325@gmail.com>
2024-06-27 13:29:27 +0900

    플랫포머 샘플 추가

---
+++ 12_Fusion_razor_madness/Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+// 플레이어의 애니메이션 관리
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator _anim;
+    private SpriteRenderer _renderer;
+    private PlayerRigidBodyMovement _movement;
+
+    void Start()
+    {
+        _movement = GetComponentInParent<PlayerRigidBodyMovement>();
+        _anim = GetComponent<Animator>();
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    void LateUpdate()
+    {
+        if (_movement.Velocity.x < -.1f)
+        {
+            _renderer.flipX = true;     // 왼쪽으로 움직이고 있으면 x플립 true
+        }else if (_movement.Velocity.x > .1f)
+        {
+            _renderer.flipX = false;    // 오른쪽으로 움직이고 있으면 x플립 false
+        }
+        // 좌우로 안움직이고 있으면 이전 플립 상태 유지
+
+        _anim.SetBool("Grounded", _movement.GetGrounded()); // 지표에 닫고 있는지를 애니메이터에게 전송
+        _anim.SetFloat("Y_Speed", _movement.Velocity.y);    // 위아래 운동량 전달
+        _anim.SetFloat("X_Speed_Abs", Mathf.Abs(_movement.Velocity.x)); // 좌우 이동량의 절대값 전달
+    }
+}
---
+++ 12_Fusion_razor_madness/Assets/Scripts/Player/PlayerData.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+using FusionUtilsEvents;
+
+// 세션에 접속한 플레이어의 정보 ( 눈에 보이지않는 데이터 )
+public class PlayerData: NetworkBehaviour
+{
+    // 플레이어 이름 (로비에서 설정한 이름)
+    [Networked]
+    public NetworkString<_16> Nick { get; set; }
+
+    // 플레이어의 인스턴스 (실제로 보이는 플레이어 오브젝트)
+    [Networked]
+    public NetworkObject Instance { get; set; }
+
+    // 델리게이트의 집합 (무엇이 등록되었는지 알 수없음) ?
+    public FusionEvent OnPlayerDataSpawnedEvent;
+
+    // 네트워크 오브젝트의 값 변경 감지용
+    private ChangeDetector _changeDetector;
+
+    // 이름 설정하는 Rpc(플레이어가 호스트에게 요청)
+    [Rpc(sources: RpcSources.InputAuthority, targets: RpcTargets.StateAuthority, HostMode = RpcHostMode.SourceIsHostPlayer)]
+    public void RPC_SetNick(string nick)
+    {
+        Nick = nick;
+    }
+
+    public override void Spawned()
+    {
+        _changeDetector = GetChangeDetector(ChangeDetector.Source.SimulationState, false);  // 네트워크 변수 변경 감지 시작
+        if (Object.HasInputAuthority)
+        {
+            string nickName = PlayerPrefs.GetString("Nick", string.Empty);  // 오너면 PlayerPrefs에서 값을 가져오기
+            RPC_SetNick(string.IsNullOrEmpty(nickName) ? $"Player {Object.InputAuthority.AsIndex}" : nickName); // 없으면 기본이름, 있으면 설정한 이름
+        }
+
+        DontDestroyOnLoad(this);    // 씬 넘어가도 삭제되지 않게 만들기
+        Runner.SetPlayerObject(Object.InputAuthority, Object);  // 러너에 플레이어 오브젝트 설정
+        OnPlayerDataSpawnedEvent?.Raise(Object.InputAuthority, Runner); // OnPlayerDataSpawnedEvent에 추가되어 있는 델리게이트 설정
+
+        if (Object.HasStateAuthority)
+        {
+            // 호스트라면 플레이어 레퍼런스와 이 게임 오브젝트를 연결하기
+            GameManager.Instance.SetPlayerDataObject(Object.InputAuthority, this);
+        }
+    }
+
+    public override void Render()
+    {
+        foreach (var change in _changeDetector.DetectChanges(this))
+        {
+            switch (change)
+            {
+                case nameof(Nick):
+                    OnPlayerDataSpawnedEvent?.Raise(Object.InputAuthority, Runner); // 이름이 바뀌면 OnPlayerDataSpwnedEvent 발생
+                    break;
+            }
+        }
+    }
+}

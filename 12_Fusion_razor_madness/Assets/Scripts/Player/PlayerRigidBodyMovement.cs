5e248eeb6757f67ce43da1111700bf8917a8b060
mob954325 <lsh954325@gmail.com>
2024-07-01 13:21:30 +0900

    플레이어 관련 주석 추가

--- 12_Fusion_razor_madness/Assets/Scripts/Player/PlayerRigidBodyMovement.cs
+++ 12_Fusion_razor_madness/Assets/Scripts/Player/PlayerRigidBodyMovement.cs
@@ -5,23 +5,37 @@ using NetworkRigidbody2D = Fusion.Addons.Physics.NetworkRigidbody2D;
 public class PlayerRigidBodyMovement : NetworkBehaviour
 {
     [Header("Movement")]
+    // 플레이어 비헤이비어 (입력이 허용되어있는지 확인)
     private PlayerBehaviour _behaviour;
+    // 바닥 레이어
     [SerializeField] private LayerMask _groundLayer;
+    // 리지드 바디
     private NetworkRigidbody2D _rb;
+    // 입력받는 클래스
     private InputController _inputController;
 
+    // 이동속도
     [SerializeField] float _speed = 10f;
+    // 점프력
     [SerializeField] float _jumpForce = 10f;
+    // 최대 이동속도 (정확히 이 값으로 최대치가 설정되는 것은 아님. 근사치로 설정됨)
     [SerializeField] float _maxVelocity = 8f;
 
+    // 일반적으로 떨어질 때의 보정값
     [SerializeField] private float fallMultiplier = 3.3f;
+    // 낮은 점프용 보정값
     [SerializeField] private float lowJumpMultiplier = 2f;
+    // 벽 슬라이딩 중일 때 떨어지는 보정 값
     private readonly float wallSlidingMultiplier = 1f;
 
-    private Vector2 _groundHorizontalDragVector = new Vector2(.1f, 1);
-    private Vector2 _airHorizontalDragVector = new Vector2(.98f, 1);
-    private Vector2 _horizontalSpeedReduceVector = new Vector2(.95f, 1);
-    private Vector2 _verticalSpeedReduceVector = new Vector2(1, .95f);
+    // 바닥 수평 마찰
+    private Vector2 _groundHorizontalDragVector = new Vector2(.1f, 1);      // 90% 감소
+    // 공주 수평 마찰력
+    private Vector2 _airHorizontalDragVector = new Vector2(.98f, 1);        // 2% 감소
+    // 최대치를 넘어섰을 때 감쇄용(수평)
+    private Vector2 _horizontalSpeedReduceVector = new Vector2(.95f, 1);    // 5% 감소
+    // 최대치를 넘어섰을 때 감쇄용(수직)
+    private Vector2 _verticalSpeedReduceVector = new Vector2(1, .95f);      // 5% 감소
 
     private Collider2D _collider;
     [Networked]
@@ -134,7 +148,7 @@ public class PlayerRigidBodyMovement : NetworkBehaviour
     {
         DetectGroundAndWalls();
 
-        if (input.GetButton(InputButton.LEFT) && _behaviour.InputsAllowed)
+        if (input.GetButton(InputButton.LEFT) && _behaviour.InputsAllowed)  // 입력이 허용된 상황에서 왼쪽 누르기
         {
             //Reset x velocity if start moving in oposite direction.
             if (_rb.Rigidbody.velocity.x > 0 && IsGrounded)
@@ -143,7 +157,7 @@ public class PlayerRigidBodyMovement : NetworkBehaviour
             }
             _rb.Rigidbody.AddForce(Vector2.left * _speed * Runner.DeltaTime, ForceMode2D.Force);
         }
-        else if (input.GetButton(InputButton.RIGHT) && _behaviour.InputsAllowed)
+        else if (input.GetButton(InputButton.RIGHT) && _behaviour.InputsAllowed)    // 입력이 허용된 상황에서 오른쪽 누르기
         {
             //Reset x velocity if start moving in oposite direction.
             if (_rb.Rigidbody.velocity.x < 0 && IsGrounded)
@@ -152,7 +166,7 @@ public class PlayerRigidBodyMovement : NetworkBehaviour
             }
             _rb.Rigidbody.AddForce(Vector2.right * _speed * Runner.DeltaTime, ForceMode2D.Force);
         }
-        else
+        else // 입력이 허용되어 있지 않거나, 좌우가 눌러지지 않았다.
         {
             //Different horizontal drags depending if grounded or not.
             if (IsGrounded)
@@ -169,12 +183,12 @@ public class PlayerRigidBodyMovement : NetworkBehaviour
         //Limit horizontal velocity
         if (Mathf.Abs(_rb.Rigidbody.velocity.x) > _maxVelocity)
         {
-            _rb.Rigidbody.velocity *= _horizontalSpeedReduceVector;
+            _rb.Rigidbody.velocity *= _horizontalSpeedReduceVector; // x에 0.95를 곱해서 살짝 줄이기
         }
 
         if (Mathf.Abs(_rb.Rigidbody.velocity.y) > _maxVelocity * 2)
         {
-            _rb.Rigidbody.velocity *= _verticalSpeedReduceVector;
+            _rb.Rigidbody.velocity *= _verticalSpeedReduceVector;   // y에 0.95를 곱해서 살짝줄이기
         }
     }
 
@@ -249,20 +263,24 @@ public class PlayerRigidBodyMovement : NetworkBehaviour
     /// <param name="input"></param>
     private void BetterJumpLogic(InputData input)
     {
-        if (IsGrounded) { return; }
-        if (_rb.Rigidbody.velocity.y < 0)
+        if (IsGrounded) { return; }         // 공중이고
+        if (_rb.Rigidbody.velocity.y < 0)   // 떨어지는 상황
         {
+            // 떨어지는 상황이다.
             if (_wallSliding && input.AxisPressed())
             {
+                // 벽에서 좌우 중 입력하고 있을 때(벽 슬라이딩 중)
                 _rb.Rigidbody.velocity += Vector2.up * Physics2D.gravity.y * (wallSlidingMultiplier - 1) * Runner.DeltaTime;
             }
             else
             {
+                // 벽이 아니거나 입력이 없을 때(벽 슬라이딩이 아닌 경우)
                 _rb.Rigidbody.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Runner.DeltaTime;
             }
         }
         else if (_rb.Rigidbody.velocity.y > 0 && !input.GetButton(InputButton.JUMP))
         {
+            // 위로 점프 중일 때 점프버튼이 안눌러져 있는 경우
             _rb.Rigidbody.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Runner.DeltaTime;
         }
     }

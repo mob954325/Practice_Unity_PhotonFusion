e044d3e5f889402848b185c504873fa7699db645
mob954325 <lsh954325@gmail.com>
2024-07-02 12:19:40 +0900

    장애물 주석 추가

--- 12_Fusion_razor_madness/Assets/Scripts/Saws/SawBlade.cs
+++ 12_Fusion_razor_madness/Assets/Scripts/Saws/SawBlade.cs
@@ -3,36 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
 
+// 스르파이트만 변경하는 클래스
 public class SawBlade : NetworkBehaviour
 {
+    // 애니메이션 스프라이트
     [SerializeField] private Sprite[] _sprites = new Sprite[2];
     protected SpriteRenderer _renderer;
     private bool _spriteController;
+
+    // 타임 체크를 위한 float
     private float _spriteTimer;
+
+    // _spriteTimer를 리셋하는 값
     private float _spriteChangeCD = .1f;
 
     public virtual void Start()
     {
         _renderer = GetComponent<SpriteRenderer>();
-        _spriteTimer = _spriteChangeCD;
+        _spriteTimer = _spriteChangeCD; // 초기값 설정
     }
 
     void Update()
     {
-        if (_spriteTimer > 0)
+        if (_spriteTimer > 0)   // _spriteTimer값이 남아있으면
         {
-            _spriteTimer -= Time.deltaTime;
+            _spriteTimer -= Time.deltaTime; // 계속 감소시키기
         }
-        else
+        else // 타이머가 만료되면
         {
-            _spriteTimer = _spriteChangeCD;
+            _spriteTimer = _spriteChangeCD; // 타이머 리셋
             ChangeSprite();
         }
     }
 
     private void ChangeSprite()
     {
-        _renderer.sprite = _sprites[_spriteController ? 0 : 1];
-        _spriteController = !_spriteController;
+        _renderer.sprite = _sprites[_spriteController ? 0 : 1]; // 0번째와 1번째를 계속 스왑
+        _spriteController = !_spriteController; 
     }
 }
